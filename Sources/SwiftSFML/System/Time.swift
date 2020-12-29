d1e35523c1b0e26676f4c363c2d4b50140b114be
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-12-29 16:59:57 -0500

    made Time Equatable and Comparable

--- Sources/SwiftSFML/System/Time.swift
+++ Sources/SwiftSFML/System/Time.swift
@@ -6,4 +6,16 @@
 import Foundation
 import CSFML
 
+/// Represents a time value.
 public typealias Time = sfTime
+
+extension Time: Equatable, Comparable {
+
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.microseconds == rhs.microseconds
+    }
+
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.microseconds < rhs.microseconds
+    }
+}
