e2537845bc9360319ed0d42a4e423d9a6fbee2f6
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-12-31 18:24:40 -0500

    merged both initialisers into one

--- Sources/SwiftSFML/Graphic/RenderWindow.swift
+++ Sources/SwiftSFML/Graphic/RenderWindow.swift
@@ -51,19 +51,14 @@ public class RenderWindow {
     /// Creates a new window with a UTF-32 title.
     public init(mode: VideoMode, title: String, 
                 style: WindowStyle = .defaultStyle, 
-                settings: inout ContextSettings) {
+                settings: ContextSettings = ContextSettings()) {
         
-        
+        var settingsCopy = settings
 
         self.window = sfRenderWindow_createUnicode(mode, 
                                                    title.utf32, 
                                                    style.rawValue, 
-                                                   &settings)
-    }
-
-    /// Creates a new window with a UTF-32 title and default window creation settings.
-    public init(mode: VideoMode, title: String, style: WindowStyle = .defaultStyle) {
-        self.window = sfRenderWindow_createUnicode(mode, title.utf32, style.rawValue, nil)
+                                                   &settingsCopy)
     }
 
     deinit {
