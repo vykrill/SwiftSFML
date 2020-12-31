7e46cf920bedf1ddc62636b3d6cfccb5c025a071
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-12-31 17:07:35 -0500

    began testing RenderWindow behaviour

--- Tests/SwiftSFMLTests/WindowTests.swift
+++ Tests/SwiftSFMLTests/WindowTests.swift
@@ -13,7 +13,17 @@ final class WindowTests: XCTestCase {
         // results.
     }
 
+    /// Test if `RenderWindow` is working properly
+    func testRenderWindowCreation() {
+        let videoMode = VideoMode(width: 640, height: 480, bitsPerPixel: 32)
+        // If we do not provide a name, the window does't appear on screen.
+        let renderWindow = RenderWindow(mode: videoMode, title: "Swift SFML")
+
+        sleep(Time(seconds: 3.0))
+    }
+
     static var allTests = [
         ("windowTestParameters", testParameters),
+        ("windowTestCreation", testRenderWindowCreation)
     ]
 }
