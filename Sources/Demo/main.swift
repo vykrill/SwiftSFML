f15f2868cff7378f740b98d2ef7a5bc8b2edce6c
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-02 22:48:57 -0500

    added a little more interactivity to the window

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -2,16 +2,26 @@ import SwiftSFML
 
 print("Hello")
 
+let defaultWidth = 640
+let defaultHeight = 480
+
 var event = Event.unknown
-var window = RenderWindow(mode: VideoMode(width: 640, height: 480, bitsPerPixel: 32), title: "SwiftSFML Demo")
+var window = RenderWindow(
+    mode: VideoMode(width: 640, height: 480, bitsPerPixel: 32), 
+    title: "SwiftSFML Demo - w: \(defaultWidth), h: \(defaultHeight) "
+)
 
 while window.isOpen {
     while window.poll(event: &event) {
         switch event {
         case .closed:
             window.close()
+        case let .resized(width, height):
+            window.setTitle("SwiftSFML Demo - w: \(width), h: \(height) ")
         default:
             break
         }
     }
+
+    
 }
