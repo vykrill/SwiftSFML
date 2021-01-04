d9cc2b13ca0390f0046cd0df716350d0c33effea
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-04 12:57:18 -0500

    added the capacity to change the background color

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -5,6 +5,8 @@ print("Hello")
 let defaultWidth = 640
 let defaultHeight = 480
 
+var currentHue: Double = 0
+
 var event = Event.unknown
 var window = RenderWindow(
     mode: VideoMode(width: 640, height: 480, bitsPerPixel: 32), 
@@ -16,13 +18,25 @@ while window.isOpen {
         switch event {
         case .closed:
             window.close()
-         case let .resized(width, height):
+        case let .resized(width, height):
             // window.setTitle(to: "SwiftSFML Demo - w: \(width), h: \(height) é".utf8CString)
             window.setTitle(to: "SwiftSFML Demo - w: \(width), h: \(height) é")
+        case let .keyPressed(data):
+            // We can change the window background with le left and right arrows.
+            switch data.code {
+            case .left:
+                currentHue -= 1
+                if currentHue < 0 { currentHue = 359}
+            case .right:
+                currentHue += 1
+                if currentHue >= 360 { currentHue = 0}
+            default:
+                break
+            }
         default:
             break
         }
     }
-    window.clear(fillColor: .red)
+    window.clear(fillColor: Color(h: currentHue, s: 1, v: 1))
     window.display()
 }
