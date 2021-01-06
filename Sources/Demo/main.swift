1d6e8e669a709199f0e080f1e794cf3e18a57e7e
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-06 14:42:42 -0500

    added a circle to the demo

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -5,12 +5,29 @@ print("Hello")
 let defaultWidth = 640
 let defaultHeight = 480
 
+/// The radius of the circle.
+let radius: Float = 50.0
+let minSize = UInt32(2 * radius)
+
 var currentHue: Double = 0
+
+var settings = ContextSettings()
+settings.antialiasingLevel = 8
+
+// Circle shape
+var circle = CircleShape(radius: radius)
+circle.origin = Vector2F(x: radius, y: radius)
+circle.position = Vector2F(x: Float(defaultWidth) / 2, y: Float(defaultHeight) / 2)
+circle.outlineColor = .black
+circle.outlineThickness = 10.0
+circle.fillColor = .white
 
 var event = Event.unknown
 var window = RenderWindow(
     mode: VideoMode(width: 640, height: 480, bitsPerPixel: 32), 
-    title: "SwiftSFML Demo - w: \(defaultWidth), h: \(defaultHeight) é"
+    title: "SwiftSFML Demo",
+    style: .defaultStyle,
+    settings: settings
 )
 
 while window.isOpen {
@@ -38,5 +55,8 @@ while window.isOpen {
         }
     }
     window.clear(fillColor: Color(h: currentHue, s: 1, v: 1))
+
+    window.draw(circle)
+
     window.display()
 }
