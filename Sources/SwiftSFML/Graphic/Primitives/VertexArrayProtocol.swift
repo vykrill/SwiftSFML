556ca09c35f6e0a731ac89038d624358cfe474f4
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-16 23:29:31 -0500

    expanded VertexArray to have acces to dimensions

--- Sources/SwiftSFML/Graphic/Primitives/VertexArrayProtocol.swift
+++ Sources/SwiftSFML/Graphic/Primitives/VertexArrayProtocol.swift
@@ -6,7 +6,28 @@
 /// Defines simple geometry that can be drawn in `RenderWindow`.
 public protocol VertexArray {
     /// The vertices composing the object.
-    var vertices: [Vertex] { get }
+    var vertices: [Vertex] { get set }
     /// How vertices are linked together.
     var type: PrimitiveType { get }
+}
+
+extension VertexArray {
+    var size: Vector2F {
+        let x = self.vertices.map({$0.position.x})
+        let y = self.vertices.map({$0.position.y})
+        
+        return Vector2F(x: (x.max() ?? 0) - (x.min() ?? 0), y: (y.max() ?? 0) - (y.min() ?? 0))
+    }
+
+    var bounds: RectF {
+        let x = self.vertices.map({$0.position.x})
+        let y = self.vertices.map({$0.position.y})
+
+        return RectF(
+            left: x.min() ?? 0,
+            top: y.min() ?? 0, 
+            width: (x.max() ?? 0) - (x.min() ?? 0), 
+            height: (y.max() ?? 0) - (y.min() ?? 0)
+        )
+    }
 }
