8a4d989648201059e4b07304ec6cdc49f82f6f73
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-16 23:32:37 -0500

    Created the Drawable protocol

--- Sources/SwiftSFML/Graphic/Shapes/Drawable.swift
+++ Sources/SwiftSFML/Graphic/Shapes/Drawable.swift
@@ -4,6 +4,36 @@
 // For SwiftSFML
 
 public protocol Drawable: VertexArray {
-    
+    var texture: Texture? { get }
+    var transform: Transform { get set }
+    var origin: Vector2F { get }
+}
+
+extension Drawable {
+
+    mutating func translate(by offset: Vector2F) {
+        self.transform.translate(by: offset)
+    }
+
+    mutating func scale(by factors: Vector2F) {
+        self.transform.scale(by: factors, withCenter: origin)
+    }
+
+    mutating func rotate(by angle: Float) {
+        self.transform.rotate(by: angle, withCenter: origin)
+    }
+
+    mutating func resetTextureRect() {
+        guard let size = self.texture?.size else {
+            return
+        }
+
+        for index in self.vertices.indices {
+            self.vertices[index].texCoords = Vector2F(
+                x: (self.vertices[index].position.x * Float(size.x)) / self.bounds.width,
+                y: (self.vertices[index].position.y * Float(size.y)) / self.bounds.height
+            )
+        }
+    }
     
 }
