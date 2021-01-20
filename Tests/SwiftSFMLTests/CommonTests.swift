c7feec3baea1c0060283b1affd8331acb6235d16
Jérémy Goyette <jeremy.goyette00@gmail.com>
2021-01-19 20:59:35 -0500

    Updated the test to compense to ending 0

--- Tests/SwiftSFMLTests/CommonTests.swift
+++ Tests/SwiftSFMLTests/CommonTests.swift
@@ -7,9 +7,11 @@ final class CommonTests: XCTestCase {
         let string = "Swift SFML"
         let scalars = string.utf32.map{Unicode.Scalar($0)!}
         // let newString = String(string.utf32.map {UnicodeScalar($0)})
-        let newString = String(String.UnicodeScalarView(scalars))
+        var newString = String(String.UnicodeScalarView(scalars))
         print(string.utf32)
         print(newString)
+        // These two strings are different because 'newString' has an null character at the end.
+        let _ = newString.popLast()
         XCTAssertEqual(string, newString)
     }
 
